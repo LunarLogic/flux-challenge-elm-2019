df3b950c624c4c765f73207a5eb5e2b68a738df3
Rafał Cieślak <rafal.cieslak@lunarlogic.io>
2019-01-17 14:08:11 +0100

    Add basic app which makes http request on init
    Co-authored-by: AkkarinForest <alexander.suminski@lunarlogic.io>
    Co-authored-by: majhogee <jo.wojtowicz@gmail.com>
    Co-authored-by: Przemysław Haptaś <przemyslawhaptas@gmail.com>

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,41 @@
 module Main exposing (main)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Http
+
+
+main =
+    Browser.element
+        { init = init
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        , update = update
+        }
+
+
+type alias Model =
+    Int
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( 0
+    , Http.get
+        { url = "http://localhost:3000/dark-jedis/3616"
+        , expect = Http.expectString GotText
+        }
+    )
+
+
+type Msg
+    = GotText (Result Http.Error String)
+
+
+update : msg -> Model -> ( Model, Cmd msg )
+update msg model =
+    ( model, Cmd.none )
 
 
 viewSith name planet =
@@ -13,7 +47,8 @@ viewSith name planet =
         ]
 
 
-main =
+view : Model -> Html msg
+view _ =
     div [ class "css-root" ]
         [ h1 [ class "css-planet-monitor" ]
             [ text "Obi-Wan currently on Tatooine" ]
